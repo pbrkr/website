8c200ac26dbc56c1af5400f8450179e416f4a139
Paul Barker <pbarker@pbarker.dev>
2020-07-11 14:35:00 +0100

    Set output directory to 'public'
    This makes it easier to publish the site with GitLab Pages.
    
    Signed-off-by: Paul Barker <pbarker@pbarker.dev>

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = 'en'
 
+OUTPUT_PATH = 'public'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
