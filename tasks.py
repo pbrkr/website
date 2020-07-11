e14d155ba2d51dd823a9110bd32244f9da0dec18
Paul Barker <pbarker@pbarker.dev>
2020-07-11 14:45:58 +0100

    Drop unnecessary invoke tasks
    Signed-off-by: Paul Barker <pbarker@pbarker.dev>

--- tasks.py
+++ tasks.py
@@ -6,7 +6,6 @@ import sys
 import datetime
 
 from invoke import task
-from invoke.util import cd
 from pelican.server import ComplexHTTPRequestHandler, RootedHTTPServer
 from pelican.settings import DEFAULT_CONFIG, get_settings_from_file
 
@@ -61,49 +60,3 @@ def serve(c):
 
     sys.stderr.write('Serving on port {port} ...\n'.format(**CONFIG))
     server.serve_forever()
-
-@task
-def reserve(c):
-    """`build`, then `serve`"""
-    build(c)
-    serve(c)
-
-@task
-def preview(c):
-    """Build production version of site"""
-    c.run('pelican -s {settings_publish}'.format(**CONFIG))
-
-@task
-def livereload(c):
-    """Automatically reload browser tab upon file modification."""
-    from livereload import Server
-    build(c)
-    server = Server()
-    # Watch the base settings file
-    server.watch(CONFIG['settings_base'], lambda: build(c))
-    # Watch content source files
-    content_file_extensions = ['.md', '.rst']
-    for extension in content_file_extensions:
-        content_blob = '{0}/**/*{1}'.format(SETTINGS['PATH'], extension)
-        server.watch(content_blob, lambda: build(c))
-    # Watch the theme's templates and static assets
-    theme_path = SETTINGS['THEME']
-    server.watch('{}/templates/*.html'.format(theme_path), lambda: build(c))
-    static_file_extensions = ['.css', '.js']
-    for extension in static_file_extensions:
-        static_file = '{0}/static/**/*{1}'.format(theme_path, extension)
-        server.watch(static_file, lambda: build(c))
-    # Serve output path on configured port
-    server.serve(port=CONFIG['port'], root=CONFIG['deploy_path'])
-
-
-@task
-def publish(c):
-    """Publish to production via rsync"""
-    c.run('pelican -s {settings_publish}'.format(**CONFIG))
-    c.run(
-        'rsync --delete --exclude ".DS_Store" -pthrvz -c '
-        '-e "ssh -p {ssh_port}" '
-        '{} {ssh_user}@{ssh_host}:{ssh_path}'.format(
-            CONFIG['deploy_path'].rstrip('/') + '/',
-            **CONFIG))
